d9362f779faece409ae9268875b1d903adaf8c2f
DonaldKellett <i.donaldl@me.com>
2015-07-14 13:44:20 +0800

    First Slide complete
    Harder than I thought :p

---
+++ introduction-ppt/assets/js/custom.js
@@ -0,0 +1,14 @@
+/*
+  custom.js
+  My custom Javascript file for my AS Physics Introductory (Slidex) Presentation
+  (c) Donald Leung.  All rights reserved.
+*/
+
+// Initialize Slidex with custom settings
+// Note: The background color should be overriden by the radial gradient set in my custom stylesheet (except for IE).
+
+slidex.init({
+  fontFamily: "'Comic Sans MS', sans-serif",
+  color: "white",
+  backgroundColor: "#222"
+});
